8a6456a1aad2936b989acf1880713fa404ef5296
MKozak <kozak_m@outlook.com>
2019-03-21 20:03:30 +0100

    multipleState

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,16 +3,23 @@ import React, {useState} from 'react'
 const Todo = props => {
 
     const [todoName, setTodoName] = useState('')
+    const [todoList, setTodoList] = useState([])
 
     const inputChangeHandler = (event) => {
         setTodoName(event.target.value)
     }
 
+    const todoAddHandler = () =>{
+        setTodoList(todoList.concat(todoName))
+    }
+
     return <React.Fragment>
         <input type="text" placeholder="todo" onChange={inputChangeHandler} value={todoName}/>
-        <button type="button">Add</button>
+        <button type="button" onClick={todoAddHandler}>Add</button>
         <ul>
-
+            {todoList.map(todo=>
+                <li key={todo}>{todo}</li>
+            )}
         </ul>
     </React.Fragment>
 }
