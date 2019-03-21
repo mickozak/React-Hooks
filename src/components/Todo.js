3c683c6956a9dacf78cc90d82a06a72f4fcf7b93
MKozak <kozak_m@outlook.com>
2019-03-21 20:18:54 +0100

    oneStateInsead

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,22 +2,28 @@ import React, {useState} from 'react'
 
 const Todo = props => {
 
-    const [todoName, setTodoName] = useState('')
-    const [todoList, setTodoList] = useState([])
+    //const [todoName, setTodoName] = useState('')
+    //const [todoList, setTodoList] = useState([])
+
+    const [todoState, setTodoState] = useState({
+        userInput: '',
+        todoList: []
+    })
 
     const inputChangeHandler = (event) => {
-        setTodoName(event.target.value)
+        //setTodoName(event.target.value)
+        setTodoState({userInput: event.target.value, todoList: todoState.todoList})
     }
 
     const todoAddHandler = () =>{
-        setTodoList(todoList.concat(todoName))
+        setTodoState({userInput: todoState.userInput, todoList: todoState.todoList.concat(todoState.userInput)})
     }
 
     return <React.Fragment>
-        <input type="text" placeholder="todo" onChange={inputChangeHandler} value={todoName}/>
+        <input type="text" placeholder="todo" onChange={inputChangeHandler} value={todoState.userInput}/>
         <button type="button" onClick={todoAddHandler}>Add</button>
         <ul>
-            {todoList.map(todo=>
+            {todoState.todoList.map(todo=>
                 <li key={todo}>{todo}</li>
             )}
         </ul>
