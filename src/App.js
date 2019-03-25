ea0d69fc4ac502c3566d46b0c49e0bab7d57177d
MKozak <kozak_m@outlook.com>
2019-03-25 19:56:10 +0100

    useContext

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import React, {useState} from 'react';
 import Todo from './components/Todo'
 import Header from './components/Header'
 import Auth from './components/Auth'
+import AuthContext from './auth.context'
 
 const app = props => {
 
 const [page, setPage] = useState('auth')
+const [authStates,setAuthStates] = useState(false)
 
 const switchPage = (pageName) => {
   setPage(pageName)
 }
 
+const login = () => {
+  setAuthStates(true)
+}
+
   return(
       <div className="App">
+      <AuthContext.Provider value={{status: authStates, login: login}}>
         <Header 
         onLoadTodos={switchPage.bind(this,'todos')} 
         onLoadAuth={switchPage.bind(this,'auth')}
         />
         {page==='auth'? <Auth/> : <Todo/>}
+      </AuthContext.Provider>
       </div>
   )
 };
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import AuthContext from '../auth.context'
 
-const auth = props => (
-    <h2>Auth Component</h2>
-)
+const auth = props => {
+
+    const auth = useContext(AuthContext)
+
+    return <button onClick={auth.login}>Log in!</button>
+}
 
 export default auth
